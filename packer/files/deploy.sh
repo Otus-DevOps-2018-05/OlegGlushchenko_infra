5017bd809219e0a34550b370de026ad7de004d98
OlegGlushchenko <glushchenko.study@gmail.com>
2018-07-15 00:01:16 +0500

    Fix deploy.sh, fix immutable.json, fix install_mongodb.sh

--- packer/files/deploy.sh
+++ packer/files/deploy.sh
@@ -3,6 +3,17 @@ cd /usr/local/bin
 git clone -b monolith https://github.com/express42/reddit.git
 cd reddit && bundle install
 
-echo -e "[Unit]\nDescription=Puma Rails Server\nAfter=network.target\n\n[Service]\nType=simple\nWorkingDirectory=/usr/local/bin/reddit\nExecStart=/usr/local/bin/puma\n[Install]\nWantedBy=multi-user.target" > /etc/systemd/system/puma.service
+cat <<EOF > /etc/systemd/system/puma.service
+[Unit]
+Description=Puma Rails Server
+After=network.target
+
+[Service]
+Type=simple
+WorkingDirectory=/usr/local/bin/reddit
+ExecStart=/usr/local/bin/puma
+[Install]
+WantedBy=multi-user.target
+EOF 
 systemctl daemon-reload
 systemctl enable puma.service
